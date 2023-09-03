fdd8efe85fb1d1e6fe70ff1575eb44046e532474
Cory <cameyer182@gmail.com>
2023-09-02 23:38:15 -0400

    final app

---
+++ src/books.js
@@ -0,0 +1,23 @@
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.webp'
+export const books = [
+  {
+    author: 'Rebecca Yarros',
+    title: 'Iron Flame',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'Stephen King',
+    title: 'Holly',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Freida McFadden',
+    title: 'The Housemaid',
+    img: img3,
+    id: 3,
+  },
+]
